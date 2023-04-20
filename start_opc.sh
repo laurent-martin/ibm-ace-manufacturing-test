42d89f0d3c55d3813d2a495835275140d11536c2
Laurent Martin <laurent.martin.l@gmail.com>
2023-04-20 11:40:53 +0200

    configure instead of start

--- start_opc.sh
+++ start_opc.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # https://github.com/Azure-Samples/iot-edge-opc-plc#command-line-reference
 
-set -x
 # folder where this script is located
 host_main_folder=$(realpath $(dirname "$0"))/
 # fixed path in the container image (WORKDIR)
@@ -12,49 +11,55 @@ container_pki_folder=${container_app_folder}pki
 host_pki_folder=${host_main_folder}pki
 # the client's certificate to add
 clt_crt=$container_pki_folder/clientCertificate.crt
+# OPC port on host, default in container is 50000
 opc_port=50000
+# web port on host, default in container is 8080, serves http://....:8080/pn.json
+web_port=8080
+# make sure host pki folder exists
 mkdir -p $host_pki_folder
-set +x
-exec podman run \
---rm \
---interactive \
---tty \
---name opcplc                 `#name of container` \
---hostname opcplc             `#set a fixed hostname as the opc server takes it to generate the certificate CN` \
---publish $opc_port:$opc_port `#port for opcua server` \
---publish 8080:8080           `#port to serve http://....:8080/pn.json` \
---volume $host_pki_folder:$container_pki_folder `# volume for persistency of certificates` \
-`# podman options before this line ========================================` \
-mcr.microsoft.com/iotedge/opc-plc:latest `# OPC UA simulator container image` \
-`# opc ua server options after this line ========================================` \
---unsecuretransport           `#enables the unsecured transport (test only)` \
---disableusernamepasswordauth `#disable username/password authentication` \
---autoaccept                  `#all certs are trusted when a connection is established` \
---showpnjsonph                `#show OPC Publisher configuration file on log, also available on the publish port` \
---addtrustedcertfile=$clt_crt `#add the client certificated to trusted certificates` \
---loglevel=debug              `#fatal, error, warn, info, debug, verbose` \
---portnum=$opc_port           `#the server port of the OPC server endpoint` \
---webport=8080                `#port to serve http://....:8080/pn.json` \
-`# simulation options after this line ----------------------------------` \
---slownodes=500               `#number of slow nodes` \
---slowrate=5                  `#rate in seconds to change slow nodes` \
+podman create \
+--name opcplc                 `#name of container`\
+--hostname opcplc             `#set a fixed hostname as the opc server takes it to generate the certificate CN`\
+--publish $opc_port:50000     `#port for opcua server`\
+--publish $web_port:8080      `#port to serve http://....:8080/pn.json`\
+--volume $host_pki_folder:$container_pki_folder `# volume for persistency of certificates`\
+`# podman options before this line ========================================`\
+mcr.microsoft.com/iotedge/opc-plc:latest `# OPC UA simulator container image`\
+`# opc ua server options after this line ========================================`\
+--addtrustedcertfile=$clt_crt `#add this client certificated to trusted certificates`\
+--autoaccept                  `#all certs are trusted when a connection is established`\
+--unsecuretransport           `#enables the unsecured transport (test only)`\
+--loglevel=debug              `#fatal, error, warn, info, debug, verbose`\
+--showpnjsonph                `#show OPC Publisher configuration file on log and activate the publish port`\
+`# simulation options after this line ----------------------------------`\
+--slownodes=500               `#number of slow nodes`\
+--slowrate=5                  `#rate in seconds to change slow nodes`\
 --slowtype=uint               `#data type of slow nodes (UInt-Double-Bool-UIntArray)`  \
---slowtypelowerbound=500      `#lower bound of data type of slow nodes` \
---slowtypeupperbound=1000     `#lower bound of data type of slow nodes` \
---slowtyperandomization=true  `#randomization of fast nodes value` \
---fastnodes=1000              `#number of fast nodes` \
---fastrate=1                  `#rate in seconds to change fast nodes` \
---fasttype=double             `#data type of fast nodes (UInt-Double-Bool-UIntArray)` \
---fasttypelowerbound=0        `#lower bound of data type of fast nodes` \
---fasttypeupperbound=100      `#lower bound of data type of fast nodes` \
---fasttyperandomization=true  `#randomization of fast nodes value` \
---fasttypestepsize=0.2        `#step or increment size of fast nodes value` \
---guidnodes=5                 `#number of nodes with deterministic GUID IDs` \
+--slowtypelowerbound=500      `#lower bound of data type of slow nodes`\
+--slowtypeupperbound=1000     `#lower bound of data type of slow nodes`\
+--slowtyperandomization=true  `#randomization of fast nodes value`\
+--fastnodes=1000              `#number of fast nodes`\
+--fastrate=1                  `#rate in seconds to change fast nodes`\
+--fasttype=double             `#data type of fast nodes (UInt-Double-Bool-UIntArray)`\
+--fasttypelowerbound=0        `#lower bound of data type of fast nodes`\
+--fasttypeupperbound=100      `#lower bound of data type of fast nodes`\
+--fasttyperandomization=true  `#randomization of fast nodes value`\
+--fasttypestepsize=0.2        `#step or increment size of fast nodes value`\
+--guidnodes=5                 `#number of nodes with deterministic GUID IDs`\
 "$@"                          `#forward additional options from command line`
 
+echo "Persistency volume: $host_pki_folder"
+echo "Container name: opcplc"
+echo "Manage container:"
+echo "podman start opcplc"
+echo "podman stop opcplc"
+echo "podman rm opcplc"
+echo "podman logs -f opcplc"
+
 #my_address=$(hostname -I|tr \  \\n|grep -v '^10\.')
-# --trustowncert                `#the own certificate is put into the trusted certificate store automatically.` \
-#--certdnsnames=${my_address}  `#add additional DNS names or IP addresses to this applications certificate` \
-# --appcertstorepath=/appdata \
-# --dontrejectunknownrevocationstatus \
-#--disablecertauth             `#disable certificate authentication` \
+#--trustowncert                `#the own certificate is put into the trusted certificate store automatically.`\
+#--certdnsnames=${my_address}  `#add additional DNS names or IP addresses to this applications certificate`\
+#--appcertstorepath=/appdata \
+#--dontrejectunknownrevocationstatus \
+#--disablecertauth             `#disable certificate authentication`\
+#--disableusernamepasswordauth `#disable username/password authentication`\
